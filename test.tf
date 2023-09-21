13fdd250924e6cfac1a5336d55d3a2c499e97c8f
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:41:01 +0000

    Backup Commit

--- test.tf
+++ test.tf
@@ -7,5 +7,17 @@ terraform {
   
 }
 provider "aws" {
-    region
+    region="Tokyo"
+    access_key="AKIAQFEIM6BXXG3BNJJP"
+    secret_key="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"
+
 }
+resource "aws_instance" "test"{
+    ami="Ubuntu Server 20.04"
+    instance_type="t2.micro"
+
+}
+output "instance_public_ip" {
+  value=aws
+}
+
